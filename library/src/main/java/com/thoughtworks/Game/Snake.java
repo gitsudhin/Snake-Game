d99c6315c1ba2c82e17a9606417de1e1551fd18c
Sreenadh S <sreeavtar@gmail.com>
2018-03-23 19:34:29 +0530

    Added method to move snake one point right

--- library/src/main/java/com/thoughtworks/Game/Snake.java
+++ library/src/main/java/com/thoughtworks/Game/Snake.java
@@ -12,4 +12,10 @@ public class Snake extends GameObject {
         screen.setObjectOnLocation(snake, snake.getxPos(), snake.getyPos());
         screen.clearALocation(snake.getxPos() + 1, snake.getyPos());
     }
+
+    public void moveRight(GameScreen screen, Snake snake) {
+        snake.setxPos(getxPos()+1);
+        screen.setObjectOnLocation(snake, snake.getxPos(), snake.getyPos());
+        screen.clearALocation(snake.getxPos() + 1, snake.getyPos());
+    }
 }
--- library/src/test/java/com/thoughtworks/Game/SnakeTest.java
+++ library/src/test/java/com/thoughtworks/Game/SnakeTest.java
@@ -1,18 +1,35 @@
 package com.thoughtworks.Game;
 
+import org.junit.Before;
 import org.junit.Test;
 
 import static org.hamcrest.core.Is.is;
 import static org.junit.Assert.assertThat;
 
 public class SnakeTest {
+
+    private Snake snake;
+
+    @Before
+    public void setUp() throws Exception {
+        snake = new Snake('@', 2, 2);
+    }
+
     @Test
     public void snakeShouldMoveLeft() {
-        Snake snake = new Snake('@', 2, 2);
         assertThat(snake.getxPos(),is(2));
         assertThat(snake.getyPos(),is(2));
         snake.moveLeft(new GameScreen(10,10),snake);
         assertThat(snake.getxPos(),is(1));
+        assertThat(snake.getyPos(),is(2));
+    }
+
+    @Test
+    public void snakeShouldMoveRight() {
+        assertThat(snake.getxPos(),is(2));
+        assertThat(snake.getyPos(),is(2));
+        snake.moveRight(new GameScreen(10,10),snake);
+        assertThat(snake.getxPos(),is(3));
         assertThat(snake.getyPos(),is(2));
     }
 }
